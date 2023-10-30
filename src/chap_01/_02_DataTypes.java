07b8a078f08ad791593db71b5ef7514ff0660ed9
baeggoose <bsj154@gmail.com>
2023-10-30 10:41:15 +0900

    Java

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("안녕");
+        System.out.println(109);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123  + 345);
+    }
+}
